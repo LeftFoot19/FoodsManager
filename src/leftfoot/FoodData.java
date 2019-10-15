9721625b71b194e9bf2dd0c3c98533d9633628ec
kaito1019 <b1803552@planet.kanazawa-it.ac.jp>
2019-10-15 15:45:05 +0900

    FoodDataのメンバーを追加

--- src/leftfoot/FoodData.java
+++ src/leftfoot/FoodData.java
@@ -1,5 +1,16 @@
 package leftfoot;
+import java.sql.Date;
+import java.util.Map;
 
 public class FoodData {
+    int productid;
+    int iniPrice;
+    int discount0;
+    int discount1;
+    String productName;
+    Date bestBeforeDate;
 
+    public FoodData(Map<String, String> Dictionary) {
+
+    }
 }
