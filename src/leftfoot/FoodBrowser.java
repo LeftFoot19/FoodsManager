efd99be0a7de164d0abaf523fea5fe8a4098c202
porideki <porideki@gmail.com>
2019-10-29 15:59:03 +0900

    FoodBrowserのフィールドを作成
    LOADのJson読み込み部分は、foodDiscitnariesへ読み込めた事前提で作成。データはHardCoder.LOADに実装

--- src/leftfoot/FoodBrowser.java
+++ src/leftfoot/FoodBrowser.java
@@ -1,5 +1,33 @@
 package leftfoot;
 
+import java.util.Dictionary;
+import java.util.List;
+
+import prottype.HardCoder;
+
 public class FoodBrowser {
+
+	public List<FoodData> foodDatas;
+
+	private FoodBrowser(List<FoodData> foodDatas) {
+		this.foodDatas = foodDatas;
+	}
+
+	public static FoodData LOAD() {
+
+		//読み込めた前提
+		List<Dictionary<String, String>> foodDictionaries = HardCoder.LOAD();
+
+		//FoodDataインスタンス生成
+
+		return null;	//実装の際は削除
+
+	}
+
+	public FoodData search(int id) {
+
+		return null;	//実装の際は削除
+
+	}
 
 }
---
+++ src/prottype/HardCoder.java
@@ -0,0 +1,23 @@
+package prottype;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
+
+public class HardCoder {
+
+	public static List<Dictionary<String, String>> LOAD() {
+
+		List<Dictionary<String, String>> dictionaries = new ArrayList<>();
+
+		//ここから手打ち
+
+
+
+		//ここまで
+
+		return dictionaries;
+
+	}
+
+}
